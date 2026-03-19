126ebf29d8c1f75e553fd0b05d0de1a54266aa6d
nakanararas <nr.nakanararas@gmail.com>
2026-03-18 23:48:07 -0700

    Adding school part of form

--- app/form/form.tsx
+++ app/form/form.tsx
@@ -1,8 +1,25 @@
 
 export default function Form() {
     return (
-        <p>
-            Testing commits for github
-        </p>
+        <div>
+            <h1 className="text-5xl"> Student Info Form </h1>
+            <p className="text-2xl"> Enter Your School: </p>
+
+            <main style={{ padding: "1rem"}}>
+                {/* <label htmlFor="school">School</label> */}
+                <input
+                    name="school"
+                    list = "schools"
+                    placeholder= "School name"
+                />
+
+                <datalist id="schools">
+                    <option value="Grossmont College"></option>
+                    <option value="Cuyamaca College"></option>
+
+                </datalist>
+            </main>
+        </div>
+        
     );
 }
