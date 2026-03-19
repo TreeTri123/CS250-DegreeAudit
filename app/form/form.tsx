b469ec8b67f43ad4ccc459e9262934989c290021
nakanararas <nr.nakanararas@gmail.com>
2026-03-19 14:02:39 -0700

    Adding majors on form

--- app/form/form.tsx
+++ app/form/form.tsx
@@ -6,7 +6,6 @@ export default function Form() {
             <p className="text-2xl"> Enter Your School: </p>
 
             <main style={{ padding: "1rem"}}>
-                {/* <label htmlFor="school">School</label> */}
                 <input
                     name="school"
                     list = "schools"
@@ -17,6 +16,29 @@ export default function Form() {
                     <option value="Grossmont College"></option>
                     <option value="Cuyamaca College"></option>
 
+                </datalist>
+            </main>
+
+            <p className="text-2xl"> Enter Your Major:</p>
+
+            <main style={{ padding: "1rem"}}>
+                <input
+                    name="major"
+                    list = "majors"
+                    placeholder= "Major"
+                />
+
+                <datalist id="majors">
+                    <option value="Computer Science"></option>
+                    <option value="Biology"></option>
+                    <option value="Business Administration"></option>
+                    <option value="English - Single Subject Teaching"></option>
+                    <option value="Economics"></option>
+                    <option value="History"></option>
+                    <option value="Liberal Studies Elementary Education"></option>
+                    <option value="Mathematics"></option>
+                    <option value="Political Science"></option>
+                    <option value="Psychology"></option>
                 </datalist>
             </main>
         </div>
